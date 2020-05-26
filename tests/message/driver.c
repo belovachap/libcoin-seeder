bd0046333b47f3698fccb49fffad3a6775cda101
Chapman Shoop <chapman.shoop@gmail.com>
2020-05-26 16:14:28 -0700

    Remove verack tests, covered by generic message tests.

--- tests/message/driver.c
+++ tests/message/driver.c
@@ -2,12 +2,16 @@
 
 #include <libcoin-seeder/message.h>
 
+
+
+#include <stdio.h>
+
 int main ()
 {
     // serialize/parse_message - empty payload
     {
-        bytes_s empty = {.buffer=NULL, .length=0};
-        message_s message = new_message("testing", empty);
+        bytes_s bytes = {.buffer=NULL, .length=0};
+        message_s message = new_message("testing", bytes);
 
         assert(message.magic == MAGIC);
         assert(message.command[0] == 't');
@@ -25,10 +29,34 @@ int main ()
         assert(message.length == 0);
         assert(message.checksum == 0xE2E0F65D);
         assert(message.payload == NULL);
+
+        free_message(message);
     }
 
     // serialize/parse_message - with payload
     {
-        message_s message;
+        byte *buffer = malloc(1);
+        buffer[0] = 0x01;
+        bytes_s bytes = {.buffer=buffer, .length=1};
+        message_s message = new_message("testing", bytes);
+
+        assert(message.magic == MAGIC);
+        assert(message.command[0] == 't');
+        assert(message.command[1] == 'e');
+        assert(message.command[2] == 's');
+        assert(message.command[3] == 't');
+        assert(message.command[4] == 'i');
+        assert(message.command[5] == 'n');
+        assert(message.command[6] == 'g');
+        assert(message.command[7] == '\0');
+        assert(message.command[8] == '\0');
+        assert(message.command[9] == '\0');
+        assert(message.command[10] == '\0');
+        assert(message.command[11] == '\0');
+        assert(message.length == 1);
+        assert(message.checksum == 0xDCCF129C);
+        assert(message.payload == buffer); // takes over buffer pointer
+
+        free_message(message);
     }
 }
